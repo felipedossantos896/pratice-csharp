8d8b0f5c0976eb90c5127f9481acdb9a8edbe255
Felipe Carneiro <felipedossantos896@gmail.com>
2021-07-31 00:27:27 -0300

    feat: generate log failure request

--- Monitor/Worker.cs
+++ Monitor/Worker.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Monitor.Helpers;
 
 namespace Monitor
 {
@@ -24,7 +27,19 @@ namespace Monitor
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                HttpStatusCode statusCode = await Requesters.GetStatusFromUrl(_sites.Url);
+                if (statusCode != HttpStatusCode.OK)
+                {
+                    string nameFile = string.Format("logfile_{0}.txt", DateTime.Now.ToString("yyyyMMdd"));
+                    string path = Path.Combine(@"C:\Users\User\Documents\", nameFile);
+                    StreamWriter logFile = new StreamWriter(path, true);
+                    string message = string.Format("O site {0} ficou fora do ar no dia {1}", _sites.Url, DateTime.Now.ToString("yyyyMMdd"));
+                    logFile.WriteLine(message);
+                    logFile.Close();
+                }
+
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                
                 try
                 {
                     await Task.Delay(1000, stoppingToken);
